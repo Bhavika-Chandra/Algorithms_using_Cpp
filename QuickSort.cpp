45059dccd116a1a8cb29c107a0f221d6dd8ae2cf
Bhavika <bhavikafeb@gmail.com>
2023-08-09 22:16:18 +0530

    Sorting in ascending order

---
+++ QuickSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void QuickSort(int a[15], int first, int last){
+    int i,j,pivot,temp;
+    if(first<last){
+        pivot=first;
+        i=first;
+        j=last;
+    while(i<j){
+        while(a[i]<=a[pivot]&&i<last){
+        i++; }
+        while(a[j]>a[pivot]){
+            j--;
+        }
+        if(i<j){
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+        }
+    }
+    temp=a[pivot];
+    a[pivot]=a[j];
+    a[j]=temp;
+    
+    QuickSort(a,first,j-1);
+    QuickSort(a,j+1,last);
+}}
+void printArray(int *a, int n){
+    cout<<"Array after sorting:";
+    for (int i = 0; i < n; i++){
+        cout<<a[i]<<" ";
+    }
+}
+
+int main() {
+    int n=10;
+    int a[]={1, 5, 3, 13, 7, 3, 2, 15, 45, 20};
+    QuickSort(a,0,n-1);
+    printArray(a, n);
+    return 0;
+}
